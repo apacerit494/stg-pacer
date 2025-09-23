90d9da30a64d54e69ce436ea75d505c9276804a1
Hakim Desyanto <hakim.desyanto@gmail.com>
2025-09-23 07:57:21 +0700

    clear timeline header number

--- app/Views/Timeline.php
+++ app/Views/Timeline.php
@@ -48,7 +48,7 @@
 
 
                 <h1>
-                    Timeline Pengajuan Sertifikasi 234
+                    Timeline Pengajuan Sertifikasi 2341
                 </h1>
                 <br>
                 <!-- SmartWizard html -->
